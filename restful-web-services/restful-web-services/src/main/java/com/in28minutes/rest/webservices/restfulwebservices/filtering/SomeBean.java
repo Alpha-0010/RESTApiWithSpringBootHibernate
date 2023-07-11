2968621350e2fcec529bc730c7a1595355576ea1
Shashwat <shashwatsinha1999@gmail.com>
2023-07-12 00:12:40 +0530

    Added filtering

---
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,44 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+// @JsonIgnoreProperties - used for static filtering...
+//@JsonIgnoreProperties({"field1","field2"})
+@JsonFilter("SomeBeanFilter")
+public class SomeBean {
+    private final String field1;
+
+    private final String field2;
+
+    // @JsonIgnore - used for static filtering...
+    // Can use either for @JsonIgnore or @JsonIgnoreProperties for Static-Filtering...
+//    @JsonIgnore
+    private final String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        super();
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean [field1=" + field1 + ", field2=" + field2 + ", field3=" + field3 + "]";
+    }
+
+}
